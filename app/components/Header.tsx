1cdf4c236b14f685b2e4d8e009fb47df8b95da39
P-Paths <preston@prestigiouspaths.com>
2025-08-25 16:07:08 -0400

    Make Stone E-Bikes logo bigger and bolder: Increase logo size to h-20 and header height to h-24 for more prominent branding

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -33,14 +33,14 @@ export const Header: React.FC = () => {
   return (
     <header className="bg-white text-black shadow-lg sticky top-0 z-50 border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-24">
           {/* Logo */}
           <div className="flex items-center">
             <Link href="/" className="cursor-pointer transition-opacity hover:opacity-80" data-testid="logo">
               <img 
                 src="/images/Logo/STONE E-BIKESLOGO.png" 
                 alt="Stone E-Bikes" 
-                className="h-12 w-auto"
+                className="h-20 w-auto"
               />
             </Link>
           </div>
